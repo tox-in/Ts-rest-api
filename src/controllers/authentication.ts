b5404eab541d692bc8e81f2a552181d35a598dbf
tox-in <habimanat78@gmail.com>
2024-09-22 12:24:03 +0200

    login functionality

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,40 @@ import express from 'express';
 import { createUser, getUserByEmail } from '../db/users';
 import { authentication, random } from '../helpers';
 
+export const login = async(req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if(user.authentication.password !== expectedHash){
+            return res.status(401).json({ message: 'Incorrect email or password' });
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('tony-ts', user.authentication.sessionToken, {domain: 'localhost', path: '/' });
+
+        return res.status(200).json(user).end();
+    } catch(error) {
+        console.log(error);
+        return res.status(400);
+    }
+}
+
 export const register = async(req: express.Request, res: express.Response) => {
     try {
         const { email, password, username } = req.body;
